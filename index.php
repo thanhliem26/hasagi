755ce51d8067620987a9cf1ab861fdfe9e922a0e
thanhliem26 <75157242+thanhliem26@users.noreply.github.com>
2020-12-02 09:45:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML/CSS/JS</title>
+</head>
+<body>
+	<h1>WELL COME TO CHANEL GAMMING TV</h1>
+	<h2>Họ và tên: Phạm Văn Liêm</h2>
+	<h2>age: 18</h2>
+	<h3>sex: male</h3>
+	<a>address:La Khê - Hà Đông</a>
+	<a>phone: 0866759002</a>
+	<img src="https://itvn.blog/wp-content/uploads/2020/07/Top-50-h%C3%ACnh-n%E1%BB%81n-m%C3%A1y-t%C3%ADnh-4k-%C4%91%E1%BA%B9p-v%C3%A0-ch%E1%BA%A5t-l%C6%B0%E1%BB%A3ng-nh%E1%BA%A5t-680x350.jpg">
+
+</body>
+</html>
